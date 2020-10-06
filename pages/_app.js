6cd2b14323d84f03c1c0a51bd54c1f92994c9bbd
Abdullah Shahid <abdullahshahid081@outlook.com>
2020-10-06 01:23:52 +0000

    p

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,7 @@ import '@hackclub/theme/fonts/reg-bold.css'
 import theme from '@hackclub/theme'
 import { ThemeProvider } from 'theme-ui'
 import ColorSwitcher from '../components/color-switcher'
-// import Head from 'next/head'
-// import Meta from '@hackclub/meta'
+
 
 export default class App extends NextApp {
   render() {
@@ -19,3 +18,15 @@ export default class App extends NextApp {
     )
   }
 }
+
+import Head from 'next/head'
+import Meta from '@hackclub/meta'
+<Meta
+  as={Head}
+  name="Hack Club" // site name
+  title="Hackathons" // page title
+  description="List of upcoming high school hackathons" // page description
+  image="https://hackathons.hackclub.com/card.png" // large summary card image URL
+  color="#ec3750" // theme color
+  manifest="/site.webmanifest" // link to site manifest
+/>
